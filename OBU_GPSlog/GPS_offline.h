031439717ecb5e75121a133d458f89b0ce0a8341
Unknown <kasraly@gmail.com>
2017-03-01 14:15:34 -0700

    Changed the log file to 23Ave

--- OBU_GPSlog/GPS_offline.h
+++ OBU_GPSlog/GPS_offline.h
@@ -1,8 +1,8 @@
 #include "gpsc_probe.h"
 
-#define GPS_RECORDING_BIN "/tmp/usb/gps_tmp.GPS"  //change this
-#define GPS_RECORDING "/tmp/usb/gps_tmp.csv"  //change this
-#define GPS_OFFLINE_BIN "/tmp/usb/gpsRecording.GPS"
+#define GPS_RECORDING_BIN "/tmp/usb/GPSlog_23Ave.GPS"  //change this
+#define GPS_RECORDING "/tmp/usb/GPSlog_23Ave.csv"  //change this
+#define GPS_OFFLINE_BIN "/tmp/usb/GPSlog_23Ave.GPS"
 
 struct GPSRecording {
     int GPSorRSSI;
